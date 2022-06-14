7bea2b952d1dc4b28cde4754856deef8b0dd6e7c
tobyw1234 <toby0977@gmail.com>
2022-06-14 15:17:53 +0100

    completed inital js for view all reviews endpoint

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react"
+import { getReviews } from "./api"
+import { useParams } from "react-router-dom"
+import { v4 as uuidv4 } from "uuid";
+const dayjs = require("dayjs");
+
+
+
+export default function Reviews(filter, setFilter) {
+  const [reviews, setReviews] = useState(    
+      [{
+          
+  }]);
+  const [isLoading, setIsLoading] = useState(false)
+
+
+  const { review_id, category } = useParams()
+  
+  
+  useEffect(() => {
+    getReviews(review_id).then((reviewsFromApi) => {
+      setReviews(reviewsFromApi);
+      setIsLoading(false);
+      
+    });
+  }, [review_id]);
+
+
+
+  if(isLoading) return <p>Loading</p>
+
+  return (
+    <div>
+      {console.log(filter)}
+      { 
+        reviews.map((review) => {
+          const dateCreated = dayjs(review.created_at)
+          return (
+            <ul key={uuidv4()}>
+              <h3 key={uuidv4()}>{review.title}</h3>
+              <li key={uuidv4()}>Category {review.category}</li>
+              <li key={uuidv4()}>
+                Created on{" "}
+                {dateCreated.$D + "/" + dateCreated.$M + "/" + dateCreated.$y}
+              </li>
+              <li key={uuidv4()}>By {review.owner}</li>
+              <button>Details</button>
+              {console.log(category, review_id)}
+            </ul>
+          );
+        }) }
+            </div>
+  )
+
+}
+
+
+
