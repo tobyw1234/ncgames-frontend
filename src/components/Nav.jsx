f2b1499ded4d96726871b643fb7d04f11a224b07
tobyw1234 <toby0977@gmail.com>
2022-07-26 19:58:50 +0100

    sdsd

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import { useState, useEffect } from "react";
 import { getCategories, getReviews } from "./api";
 
-
-
-
-export default function Nav({ filter, setFilter, setReviews,reviews }) {
+export default function Nav({ filter, setFilter, setReviews, reviews }) {
   const [categories, setCategories] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -18,34 +15,50 @@ export default function Nav({ filter, setFilter, setReviews,reviews }) {
   }, []);
 
   useEffect(() => {
+    setIsLoading(true);
     if (filter) {
       const filteredReviews = reviews.filter(
         (review) => review.category === filter
       );
       setReviews(filteredReviews);
-   
-    } 
+      const filteredCats = categories.filter(
+        (category) => category.slug === filter
+      );
+      filteredCats.push({slug:"All Categories"})
+
+      setCategories(filteredCats);
+      setIsLoading(false);
+    }
   }, [filter]);
 
   function resetReviews() {
     setIsLoading(true);
-    getReviews()
-      
-      .then((reviewsFromApi) => {
-        setReviews(reviewsFromApi);
-        setIsLoading(false);
-      })
-      getCategories().then((categoriesFromApi) => {
-        setCategories(categoriesFromApi);
-        navigate("/");
-        setIsLoading(false);
-      });
+    console.log(categories);
+    getReviews().then((reviewsFromApi) => {
+      setReviews(reviewsFromApi);
+      setIsLoading(false);
+    });
+    getCategories().then((categoriesFromApi) => {
+      setCategories(categoriesFromApi);
+      navigate("/");
+      setIsLoading(false);
+    });
   }
-  const navigate = useNavigate()
+
+  const navigate = useNavigate();
   function handleDropDown(e) {
-    navigate(`category/${e.target.value}`);
-    setFilter(e.target.value)
-    setCategories([e.target.value]);
+    setFilter(e.target.value);
+    if (e.target.value === "All Categories") {
+      navigate("/")
+      resetReviews();
+    } else {
+    
+      navigate(`category/${e.target.value}`)
+    }
+    
+    console.log([e.target.value]);
+    console.log(categories);
+    
   }
 
   if (isLoading) return <p>Loading</p>;
@@ -56,16 +69,19 @@ export default function Nav({ filter, setFilter, setReviews,reviews }) {
         Sort by Category
         <select key={uuidv4()} onChange={(e) => handleDropDown(e)}>
           {categories.map((category) => (
-           
-              <option key={uuidv4()} value={category.slug} onClick={()=>{navigate("/testpath")}}>
-                {category.slug}
-              </option>
-            
+            <option
+              key={uuidv4()}
+              value={category.slug}
+              onClick={() => {
+                navigate();
+              }}>
+              {category.slug}
+            </option>
           ))}
         </select>
       </label>
 
-      <button onClick={(e) => resetReviews()}>reset filter</button>
+      <button id={uuidv4()} onClick={(e) => resetReviews()}>Home</button>
     </>
   );
 }
